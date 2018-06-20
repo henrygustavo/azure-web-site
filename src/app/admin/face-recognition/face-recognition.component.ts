02883df86ad0d432b94842c1863c4cf80e7f6a3f
henrygustavo <henrygustavof@gmail.com>
2018-06-19 23:27:27 -0500

    Adding block UI

--- src/app/admin/face-recognition/face-recognition.component.ts
+++ src/app/admin/face-recognition/face-recognition.component.ts
@@ -3,6 +3,7 @@ import { MenuService } from '../../shared/services/menu.service';
 import { FaceService } from '../services/face.service';
 import { Face } from '../models/face';
 import { MessageAlertHandleService } from '../../shared/services/message-alert.service';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
 
 @Component({
   selector: 'app-face-recognition',
@@ -10,21 +11,29 @@ import { MessageAlertHandleService } from '../../shared/services/message-alert.s
   styleUrls: ['./face-recognition.component.css']
 })
 export class FaceRecognitionComponent implements OnInit {
+  @BlockUI() blockUI: NgBlockUI;
 
   constructor( private _menuService: MenuService,
                private _messageAlertHandleService: MessageAlertHandleService,
                private _faceService: FaceService) { }
 
   ngOnInit() {
-
+    this.blockUI.start();
     this._menuService.selectMenuItem('faces');
+    this.loadFaces();
+
+  }
+
+  loadFaces(): void {
 
     this._faceService.get().subscribe(
       (response: Face) => {
             console.log(response);
+            this.blockUI.stop();
       },
       (error: any) => {
                   this._messageAlertHandleService.handleError(error);
+                  this.blockUI.stop();
       });
   }
 
--- src/app/admin/image-recognition/image-recognition.component.ts
+++ src/app/admin/image-recognition/image-recognition.component.ts
@@ -3,6 +3,7 @@ import { MenuService } from '../../shared/services/menu.service';
 import { ImageService } from '../services/image.service';
 import { Image } from '../models/image';
 import { MessageAlertHandleService } from '../../shared/services/message-alert.service';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
 
 @Component({
   selector: 'app-image-recognition',
@@ -10,24 +11,32 @@ import { MessageAlertHandleService } from '../../shared/services/message-alert.s
   styleUrls: ['./image-recognition.component.css']
 })
 export class ImageRecognitionComponent implements OnInit {
+  @BlockUI() blockUI: NgBlockUI;
 
   constructor(private _menuService: MenuService,
-              private _messageAlertHandleService: MessageAlertHandleService,
-              private _imageService: ImageService) { }
+    private _messageAlertHandleService: MessageAlertHandleService,
+    private _imageService: ImageService) { }
 
   ngOnInit() {
 
     this._menuService.selectMenuItem('images');
 
+    this.loadImages();
+  }
+
+  loadImages(): void {
+
+    this.blockUI.start();
     this._imageService.get().subscribe(
       (response: Image) => {
-            console.log(response);
+        console.log(response);
+        this.blockUI.stop();
       },
       (error: any) => {
-                  this._messageAlertHandleService.handleError(error);
+        this._messageAlertHandleService.handleError(error);
+        this.blockUI.stop();
       }
-  );
-
+    );
   }
 
 }
